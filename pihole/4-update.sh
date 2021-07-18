79e288af2b79ffa0e501caf3358c722e6439b75e
Hans Song <hans.m.song@gmail.com>
2021-07-18 14:39:52 +0000

    feat(pihole): update script

---
+++ pihole/4-update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+adlist=$(curl https://v.firebog.net/hosts/lists.php\?type\=tick)
+
+pod_name=$(kubectl get pod -n pihole -o name | head -n 1)
+
+db="/etc/pihole/gravity.db"
+
+for address in $(echo $adlist); do
+  insertions="$insertions
+  INSERT OR IGNORE INTO adlist (address) VALUES (\"$address\");
+  "
+done
+
+kubectl exec $pod_name -n pihole -- \
+sqlite3 $db -batch <<EOF
+  $insertions
+  .exit
+EOF
+
+kubectl exec $pod_name -n pihole -- pihole -g
