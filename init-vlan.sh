eaa1dbc89fa3c139496e4788e4332b542b30362f
Hans Song <hans.m.song@gmail.com>
2022-06-19 05:32:00 +0000

    add runnerdeployment

--- init-vlan.sh
+++ init-vlan.sh
@@ -9,7 +9,7 @@ IP_RANGE="192.168.1.248/29" # 248-255
 MAC_ADDRESS="A2:73:D3:1C:1D:FC"
 sleep 1s
 
-sudo ip link add ${VNIC} address 42:42:42:42:42:42 link ${NIC_NAME} type macvlan mode bridge
+sudo ip link add ${VNIC} address ${MAC_ADDRESS} link ${NIC_NAME} type macvlan mode bridge
 sudo ip addr add ${STATIC_IP}/32 dev ${VNIC}
 sudo ip link set ${VNIC} up
 sudo ip route add ${IP_RANGE} dev ${VNIC}
